d9fd9d069f9553db39315a41aa7a511bb84553dd
Wahid Jamadar <wahidjamadar2020@gmail.com>
2025-02-11 09:30:27 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -3,5 +3,10 @@
 a=10
 b=12
 c=a+b
-
-print(c)
+print(f"the addition of {a},{b} is {c}")
+c=a-b
+print(f"the subtraction of {a},{b} is {c}")
+c=a/b
+print(f"the division of {a},{b} is {c}")
+c=a*b
+print(f"the multiplication of {a},{b} is {c}")
